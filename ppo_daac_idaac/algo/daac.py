fixed sinkhorn backprop

ppo_daac_idaac/algo/daac.py
@@ -107,16 +107,15 @@ class DAAC():
                         self.ctrl.protos.weight.data.copy_(C)
 
                         v_clust = F.normalize(v_clust, dim=1, p=2)
-                        w_clust = F.normalize(v_clust, dim=1, p=2)
+                        w_clust = v_clust # F.normalize(w_clust, dim=1, p=2)
 
                         scores_v = self.ctrl.protos(v_clust)
                         log_p = F.log_softmax(scores_v / self.ctrl.temp, dim=1)
 
                         scores_v_target = self.ctrl.protos(v_clust)
                         scores_w_target = self.ctrl.protos(w_clust)
-                        with torch.no_grad():
-                            q_target = self.ctrl.sinkhorn(scores_w_target)
-                        proto_loss = -(q_target * log_p).sum(axis=1).mean()
+                        q_target = self.ctrl.sinkhorn(scores_w_target)
+                        proto_loss = -(q_target.detach() * log_p).sum(axis=1).mean()
 
                         # MYOW
                         dist = compute_distance(self.ctrl.protos.weight.data.clone().T)
@@ -142,6 +141,7 @@ class DAAC():
                             myow_loss += cos_loss(v_pred, nearby_vec).mean()
 
                         ctrl_loss = proto_loss + myow_loss
+                        print(proto_loss, myow_loss, ctrl_loss)
 
                         self.ctrl_optimizer.zero_grad()
                         ctrl_loss.backward()

train.py
@@ -221,10 +221,10 @@ def train(args):
             except OSError:
                 pass
 
-            torch.save([
-                actor_critic,
-                getattr(envs, 'ob_rms', None)
-            ], os.path.join(args.save_dir, "agent{}.pt".format(log_file))) 
+            # torch.save([
+            #     actor_critic,
+            #     getattr(envs, 'ob_rms', None)
+            # ], os.path.join(args.save_dir, "agent{}.pt".format(log_file))) 
 
         # Save Logs
         total_num_steps = (j + 1) * args.num_processes * args.num_steps