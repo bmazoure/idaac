fixed ctrl loss re-scaling+ latent mlp structure

ppo_daac_idaac/algo/daac.py
@@ -102,12 +102,12 @@ class DAAC():
                         # z: cluster_len x ((n_timesteps-cluster_len) * n_processes) x n_rkhs
                         z_seq = self.actor_critic.base(obs_seq.reshape(-1,*obs_batch.size()[1:]))[1].reshape(*obs_seq.size()[:2], -1)
                         v_clust, w_clust, v_pred, w_pred = self.ctrl(z_seq, actions_seq, returns_seq)
-                        C = self.ctrl.protos.weight.data.clone()
-                        C = F.normalize(C, dim=1, p=2)
-                        self.ctrl.protos.weight.data.copy_(C)
+                        # C = self.ctrl.protos.weight.data.clone()
+                        # C = F.normalize(C, dim=1, p=2)
+                        # self.ctrl.protos.weight.data.copy_(C)
 
-                        v_clust = F.normalize(v_clust, dim=1, p=2)
-                        w_clust = v_clust # F.normalize(w_clust, dim=1, p=2)
+                        # v_clust = F.normalize(v_clust, dim=1, p=2)
+                        # w_clust = F.normalize(w_clust, dim=1, p=2)
 
                         scores_v = self.ctrl.protos(v_clust)
                         log_p = F.log_softmax(scores_v / self.ctrl.temp, dim=1)

ppo_daac_idaac/model.py
@@ -432,10 +432,10 @@ class CTRL(nn.Module):
         z = ((1 + gamma_a) * z_state + beta_a).permute(1,0,2).reshape(z_state.shape[1], -1)
         z = self.concat_mlp(z)
 
-        v_clust = self.v_clust_mlp(z)
-        w_clust = v_clust # self.w_clust_mlp(v_clust)
+        v_clust = z # self.v_clust_mlp(z)
+        w_clust = self.w_clust_mlp(v_clust)
 
-        v_pred = self.v_pred_mlp(z)
+        v_pred = z # self.v_pred_mlp(z)
         w_pred = self.w_pred_mlp(v_pred)
 
         return v_clust, w_clust, v_pred, w_pred